4a67692d38992023fdf58cba4f073d7004839359
Bun5 <bun5gza@gmail.com>
2023-05-25 00:28:59 +0700

    update statusDetail + responsive

--- components/StatusDetail.js
+++ components/StatusDetail.js
@@ -1,6 +1,7 @@
 import { fetchData } from "@/backend/firebase";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import Image from "next/image";
 
 async function getData() {
     const data = await fetchData();
@@ -18,30 +19,60 @@ export default function StatusDetail() {
     }
 
     return (
-        <div className="w-5/12 h-auto flex flex-col flex-wrap">
-            <h2 className="bg-[#255c2a] flex justify-center items-center text-2xl text-white flex-grow">
-                {`⏰ Last update: ${
+        <div className="w-5/12 h-auto flex flex-col flex-wrap text-md md:text-lg lg:text-xl xl:text-2xl">
+            <h2 className="bg-[#255c2a] flex justify-center items-center text-white flex-grow">
+                <Image className="mx-2"
+                    src={"/clock.png"}
+                    width={25}
+                    height={25}
+                    alt="header image"
+                />
+                {`Last update: ${
                     isLoading
                         ? "Loading..."
                         : getTimeString(data[data.length - 1].timestamp)
                 }`}
             </h2>
-            <p className="text-2xl bg-[#c6c6c6] flex flex-grow items-center">
+            <p className="bg-[#c6c6c6] flex flex-grow items-center pl-2">
+                <Image className="mr-2"
+                    src={"/temp.png"}
+                    width={25}
+                    height={25}
+                    alt="header image"
+                />
                 {`Air Temperature: ${
                     isLoading ? "Loading..." : data[data.length - 1].temp
                 } °C`}
             </p>
-            <p className="text-2xl bg-[#dadada] flex flex-grow items-center">
+            <p className="bg-[#dadada] flex flex-grow items-center pl-2">
+                <Image className="mr-2"
+                    src={"/waterdrop.png"}
+                    width={25}
+                    height={25}
+                    alt="header image"
+                />
                 {`Air Humidity: ${
                     isLoading ? "Loading..." : data[data.length - 1].humid
                 } %`}
             </p>
-            <p className="text-2xl bg-[#c6c6c6] flex flex-grow items-center">
+            <p className="bg-[#c6c6c6] flex flex-grow items-center pl-2">
+                <Image className="mr-2"
+                    src={"/plant.png"}
+                    width={25}
+                    height={25}
+                    alt="header image"
+                />
                 {`Soil Humidity: ${
                     isLoading ? "Loading..." : data[data.length - 1].soilHumid
                 } %`}
             </p>
-            <p className="text-2xl bg-[#dadada] flex flex-grow items-center">
+            <p className="bg-[#dadada] flex flex-grow items-center pl-2">
+                <Image className="mr-2"
+                    src={"/sun.png"}
+                    width={25}
+                    height={25}
+                    alt="header image"
+                />
                 {`Light Intensity: ${
                     isLoading ? "Loading..." : data[data.length - 1].lightLevel
                 } %`}
