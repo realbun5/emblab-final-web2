250c3a9f63b6937211de6817abc573eac8d3066b
Bun5 <bun5gza@gmail.com>
2023-05-25 15:25:40 +0700

    fixing design

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+          <title>Plantmeter</title>
+          <link href="https://fonts.googleapis.com/css2?family=Aleo&display=swap" rel="stylesheet" />
+      </Head>      
+      <body className="bg-[#e3e3e3] h-100%">
         <Main />
         <NextScript />
       </body>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import StatusDetail from "@/components/StatusDetail";
 import StatusGraph from "../components/StatusGraph";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
     return (
-        <main className="bg-[#e3e3e3] h-100%">
+        <main>
             {/* top bar */}
             <div className="bg-[#255c2a]">
                 <h1 className="flex justify-center font-aleo py-5 text-5xl text-white">
@@ -19,17 +20,31 @@ export default function Home() {
                 </h1>
                 <div className="flex gap-5 justify-center pb-4 font-aleo">
                     {/* overall status */}
-                    <div className="w-5/12 h-64 flex flex-col flex-wrap">
-                        <h2 className="bg-[#d6aa0f] flex justify-center font-aleo text-2xl text-wihte">
+                    <div className="w-5/12 h-auto flex flex-col flex-wrap">
+                        <h2 className="bg-[#d6aa0f] flex flex-grow justify-center items-center font-aleo text-white text-md md:text-lg lg:text-xl xl:text-2xl">
                             Overall Status: ...
                         </h2>
+                        <Image className="flex flex-grow items-center mx-auto"
+                            src={"/plant.png"}
+                            width={200}
+                            height={200}
+                            alt="header image"
+                        />
                     </div>
                     {/* stats */}
                     <StatusDetail className="font-aleo"/>
                 </div>
             </div>
             {/* graph */}
-            <StatusGraph />
+            <div className="px-7">
+                <h1 className="flex justify-left font-aleo pt-4 pb-3 text-4xl text-black">
+                        {" "}
+                        Summary{" "}
+                </h1>
+                <div className="bg-[#c6c6c6] px-2 mb-6">
+                    <StatusGraph /> 
+                </div>
+            </div>
         </main>
     );
 }
